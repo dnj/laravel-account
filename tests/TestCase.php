3a1e3702713927672832cf17a4e96478f0d32a30
Amir H. Yeganemehr <yeganemehr@yahoo.com>
2023-01-08 18:22:22 +0330

    Fix helper methods for tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -8,12 +8,9 @@ use dnj\Account\Contracts\IAccountManager;
 use dnj\Account\Contracts\IHoldingManager;
 use dnj\Account\Contracts\ITransactionManager;
 use dnj\Account\HoldingManager;
-use dnj\Account\Models\Account;
 use dnj\Account\Tests\Models\User;
 use dnj\Account\TransactionManager;
-use dnj\Currency\Contracts\ICurrency;
 use dnj\Currency\Contracts\ICurrencyManager;
-use dnj\Currency\Contracts\RoundingBehaviour;
 use dnj\Currency\CurrencyServiceProvider;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
@@ -58,29 +55,5 @@ class TestCase extends \Orchestra\Testbench\TestCase
     public function getHoldingManager(): HoldingManager
     {
         return $this->app->make(IHoldingManager::class);
-    }
-
-    public function createUSD(): ICurrency
-    {
-        return $this->getCurrencyManager()
-            ->create('USD', 'US Dollar', '$', '', RoundingBehaviour::CEIL, 2);
-    }
-
-    public function createEUR(): ICurrency
-    {
-        return $this->getCurrencyManager()
-            ->create('USD', 'US Dollar', '$', '', RoundingBehaviour::CEIL, 2);
-    }
-
-    public function createUSDAccount(ICurrency $USD, ?int $userId = null): Account
-    {
-        return $this->getAccountManager()
-            ->create('USD Reserve', $USD->getID(), $userId);
-    }
-
-    public function createEURAccount(ICurrency $EUR, ?int $userId = null): Account
-    {
-        return $this->getAccountManager()
-            ->create('EUR Reserve', $EUR->getID(), $userId);
     }
 }
